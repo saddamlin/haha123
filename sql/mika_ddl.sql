439654bcdd7bf57809e2e7a2ac82847bd5bd92b7
saddamlin <saddam0425@hotmail.com>
2018-03-21 00:32:54 +0800

    update sql

--- sql/mika_ddl.sql
+++ sql/mika_ddl.sql
@@ -1,7 +1,10 @@
+###### Users ######
 -- USER_INFO
 CREATE TABLE USER_INFO
 (
 USER_ID integer primary key,
+USER_AVATAR blob,
+USER_SIGNATURE text,
 CREATED_DATE timestamp,
 LAST_MODIFIED_DATE timestamp
 );
@@ -15,70 +18,93 @@ CREATED_DATE timestamp,
 LAST_MODIFIED_DATE timestamp
 );
 
--- USER_ELEMENT
-CREATE TABLE USER_ELEMENT_SUMMARY
+##### Backpack #####
+
+CREATE TABLE USER_ELEMENT_BACKPACK
 (
-USER_ID integer primary key,
 ELEMENT_ID integer primary key,
-QUANTITY integer,
+USER_ID integer primary key,
+ELEMENT_TYPE int4,
+ELEMENT_STATUS int4,
 CREATED_DATE timestamp,
 LAST_MODIFIED_DATE timestamp
 );
 
--- USER_ELEMENT_HIST
-CREATE TABLE USER_ELEMENT_OBTAINED
+CREATE TABLE MINE_ELEMENT_DETAILS
 (
-USER_ID integer primary key,
 ELEMENT_ID integer primary key,
-QUANTITY integer,
-OBTAINED_DATE timestamp,
-QUEST_ID integer,
+MINT_TYPE int4,
 CREATED_DATE timestamp,
 LAST_MODIFIED_DATE timestamp
 );
 
-CREATE TABLE USER_ELEMENT_TRANSFORMED
+CREATE TABLE USER_SEED_BACKPACK
 (
+SEED_ID integer primary key,
 USER_ID integer primary key,
+SEED_TYPE int4,
+CREATED_DATE timestamp,
+LAST_MODIFIED_DATE timestamp
+);
+
+CREATE TABLE USER_FRUIT_BACKPACK
+(
+FRUIT_ID integer primary key,
+USER_ID integer primary key,
+FRUIT_STATUS int4,
+FRUIT_TYPE int4,
+CREATED_DATE timestamp,
+LAST_MODIFIED_DATE timestamp
+);
+
+
+###### Plant ######
+
+CREATE TABLE PLANT_INFO
+(
+PLANT_ID integer primary key,
+SEED_ID integer,
+USER_ID integer,
+PLANT_TYPE int4,
+PLANT_STATUS int4,
+CREATED_DATE timestamp,
+LAST_MODIFIED_DATE timestamp
+);
+
+CREATE TABLE PLANT_ELEMENT
+(
+PLANT_ID integer primary key,
 ELEMENT_ID integer primary key,
 QUANTITY integer,
 TRANSFORMED_DATE timestamp,
-FLOWER_ID integer,
 CREATED_DATE timestamp,
 LAST_MODIFIED_DATE timestamp
 );
 
--- FLOWER
-CREATE TABLE FLOWER_INFO
+CREATE TABLE PLANT_FRUIT
 (
-FLOWER_ID integer primary key,
-USER_ID integer,
-FLOWER_TYPE int4,
-CREATED_DATE timestamp,
-LAST_MODIFIED_DATE timestamp
-);
-
--- SEED
-
-CREATE TABLE SEED_INFO
-(
-SEED_ID integer primary key,
-USER_ID integer,
-SEED_TYPE int4,
-SOURCE int4,
-IS_FLOWERING boolean,
-CREATED_DATE timestamp,
-LAST_MODIFIED_DATE timestamp
-);
-
-CREATE TABLE SEED_ELEMENT_SUMMARY
-(
-SEED_ID integer primary key,
-USER_ID integer,
-ELEMENT_ID integer,
+PLANT_ID integer primary key,
+FRUIT_ID integer primary key,
 QUANTITY integer,
+FRUTION_DATE timestamp,
 CREATED_DATE timestamp,
-LAST_MODIFIED_DATE timestamp
+LAST_MODIFIED_DATE timestamp 
+);
+
+CREATE TABLE FRUIT_ELEMENTW
+(
+FRUIT_ID integer primary key,
+ELEMENT_ID integer primary key,
+CREATED_DATE timestamp,
+LAST_MODIFIED_DATE timestamp 
+);
+
+CREATE TABLE GENE_ELEMENT_DETAILS
+(
+ELEMENT_ID integer primary key,
+GENE_TYPE int4,
+CREATED_DATE timestamp,
+LAST_MODIFIED_DATE timestamp 
 );
 
 -- QUEST_INFO
@@ -86,9 +112,11 @@ CREATE TABLE QUEST_INFO
 (
 QUEST_ID integer primary key,
 USER_ID integer,
+FRUIT_ID integer,
 START_DATE timestamp,
 END_DATE timestamp,
 IS_SUCCESS boolean,
+GENE_ELEMENT_ID integer,
 CREATED_DATE timestamp,
 LAST_MODIFIED_DATE timestamp
 );
